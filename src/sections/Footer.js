de272a94291ed658f1dff0a39c63940f1e9f7633
TerZhang <terzhang@hotmail.com>
2020-02-01 03:07:48 -0500

    implement the Footer section

--- src/sections/Footer.js
+++ src/sections/Footer.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { lazy } from 'react';
+import { Stack } from '@chakra-ui/core';
+const SvgStairs = lazy(() => import('../components/SvgComponents/Stairs'));
+const SvgDoor = lazy(() => import('../components/SvgComponents/Door'));
+const SvgHangingLight = lazy(() =>
+  import('../components/SvgComponents/HangingLight')
+);
 
-const Footer = () => {
-  return <div></div>;
+const Footer = (props) => {
+  return (
+    <Stack
+      w='full'
+      backgroundColor='black'
+      justifyContent='space-between'
+      isInline
+      {...props}
+    >
+      <SvgDoor style={{ alignSelf: 'flex-end' }} />
+      <SvgHangingLight style={{ alignSelf: 'flex-start' }} />
+      <SvgStairs />
+    </Stack>
+  );
 };
 
 export default Footer;
