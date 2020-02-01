0b88745da3902bde914863c6c5fcb360e1d58994
TerZhang <terzhang@hotmail.com>
2020-02-01 03:21:23 -0500

    fixing spacing and increase fontsize

--- src/sections/Contact.js
+++ src/sections/Contact.js
@@ -10,8 +10,8 @@ const SvgBoat = lazy(() => import('../components/SvgComponents/ContactsBoat'));
 
 const Content = (props) => {
   return (
-    <Stack {...props}>
-      <Text as='h2' id='boat-label'>
+    <Stack alignItems='center' {...props}>
+      <Text as='h2' fontSize='5xl' id='boat-label'>
         There's a boat load of methods to find me
       </Text>
       <SvgBoat labelledby='boat-label' />
@@ -23,7 +23,7 @@ const Contact = () => {
   return (
     <Stack as='section' isInline position='relative' w='full' h='full'>
       <SvgLighthouse />
-      <Content />
+      <Content alignSelf='flex-end' />
       <SvgVertical
         height='100%'
         preserveAspectRatio='none'
