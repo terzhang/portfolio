fdfd5f23425ea88d42c19e5bacc8ab1739726576
TerZhang <terzhang@hotmail.com>
2020-02-22 22:40:16 -0500

    Render skill list as tags

--- src/sections/SkillsAndExperience.js
+++ src/sections/SkillsAndExperience.js
@@ -1,5 +1,14 @@
 import React, { lazy } from 'react';
-import { Stack, Heading, Text, List, ListItem } from '@chakra-ui/core';
+import {
+  Stack,
+  Heading,
+  Text,
+  List,
+  ListItem,
+  Avatar,
+  Tag,
+  TagLabel,
+} from '@chakra-ui/core';
 const SvgFrameworks = lazy(() =>
   import('../components/SvgComponents/Frameworks')
 );
@@ -9,21 +18,72 @@ const SvgVertical = lazy(() =>
 
 const ExperienceItem = ({ title, date = '', description = '' }) => (
   <ListItem key={title + date}>
-    <Text>
+    <Text as='p'>
       <Text as='span' fontSize='4xl'>
         {title}
       </Text>
       <Text as='span' fontSize='lg' color='gray.500'>{` (${date})`}</Text>
-      <Text>{description}</Text>
     </Text>
+    <Text as='p'>{description}</Text>
   </ListItem>
 );
+
+const SkillTags = ({ skillsObj }) => {
+  // check any resource is given
+  if (!skillsObj) {
+    return null;
+  }
+
+  // method pick a color index randomly
+  const colors = ['red', 'blue', 'green', 'purple'];
+  const pickColor = () => colors[Math.floor(Math.random() * colors.length)];
+
+  // map out the given avatar object into an array of tags
+  return (
+    <List display='inline-block'>
+      {skillsObj.map((avatar) => (
+        <ListItem key={avatar.name} display='inline' mr={2}>
+          <Tag variantColor={pickColor()} rounded='full'>
+            <Avatar
+              {...avatar}
+              background='none'
+              size='xs'
+              ml={-1}
+              mr={2}
+              object-fit='contain'
+            />
+            <TagLabel>{avatar.name}</TagLabel>
+          </Tag>
+        </ListItem>
+      ))}
+    </List>
+  );
+};
+
+const skillsObj = [
+  {
+    src: 'https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg',
+    name: 'React',
+  },
+  {
+    src: 'https://git-scm.com/images/logos/downloads/Git-Icon-1788C.png',
+    name: 'Git',
+  },
+  {
+    src: 'https://cdn.worldvectorlogo.com/logos/nodejs-icon.svg',
+    name: 'Node JS',
+  },
+  {
+    src: 'https://upload.wikimedia.org/wikipedia/commons/6/64/Expressjs.png',
+    name: 'Express',
+  },
+];
 
 const Content = (props) => (
   <Stack spacing={4} justifyContent='space-evenly' h='full' {...props}>
     <Stack>
       <Heading size='lg'>I'm well versed in</Heading>
-      <Text alignSelf='center'>React - Git - NodeJs - Express</Text>
+      <SkillTags skillsObj={skillsObj} />
     </Stack>
     <Heading size='lg' id='experience'>
       Work Experience
