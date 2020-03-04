f86e8b0f4ced567c9ef7b9175afc400557403efe
TerZhang <terzhang@hotmail.com>
2020-03-04 18:32:00 -0500

    Remove need for useSize in favor of react portal

--- src/sections/index.js
+++ src/sections/index.js
@@ -1,4 +1,5 @@
-import React, { lazy, useRef } from 'react';
+import React, { lazy, useState } from 'react';
+import { createPortal } from 'react-dom';
 import { Box, useColorMode } from '@chakra-ui/core';
 
 import Home from './Home';
@@ -8,42 +9,45 @@ import SkillsAndExperience from './SkillsAndExperience';
 import CurveDivider from '../components/CurveDivider';
 import { keyframes } from '@emotion/core';
 import styled from '@emotion/styled';
-import useSize from '../hooks/useSize';
 
 const HomeCurveDivider = lazy(() => import('../components/HomeCurveDivider'));
 const SvgKitty = lazy(() => import('../components/SvgComponents/Kitty'));
 
+const xPath = keyframes`
+to {
+  transform: translateX(500px);
+}
+`;
+
+const yPath = keyframes`
+0% {
+  transform: translateY(0);
+}
+100% {
+  transform: translateY(500px);
+}
+`;
+
+const Kitty = styled(SvgKitty)`
+  z-index: 99;
+  position: absolute;
+  left: 0;
+  margin-left: -100px;
+  animation: ${yPath} 3s ease-out forwards;
+  :after {
+    content: ${''};
+    display: block;
+    animation: ${xPath} 3s ease-in forwards;
+  }
+`;
+
+const Portal = ({ children, to }) => {
+  return to && createPortal(children, to);
+};
+
 export default function Sections() {
   const { colorMode } = useColorMode();
-  const homeRef = useRef();
-  const homeSize = useSize(homeRef);
-
-  const xPath = keyframes`
-  to {
-    transform: translateX(500px);
-  }
-  `;
-
-  const yPath = keyframes`
-  0% {
-    transform: translateY(0);
-  }
-  100% {
-    transform: translateY(${homeSize.height}px);
-  }
-  `;
-
-  const Kitty = styled(SvgKitty)`
-    z-index: 99;
-    position: absolute;
-    margin-left: -100px;
-    animation: ${yPath} 3s ease-out forwards;
-    :after {
-      content: ${''};
-      display: block;
-      animation: ${xPath} 3s ease-in forwards;
-    }
-  `;
+  const [section, setSection] = useState(null);
 
   return (
     <Box
@@ -57,14 +61,16 @@ export default function Sections() {
       position='relative'
       px='10%'
     >
-      <Kitty />
-      <Home ref={homeRef} />
+      <Home ref={(c) => setSection(c)} />
       <HomeCurveDivider />
       <SkillsAndExperience />
       <CurveDivider />
       <PastWorks />
       <CurveDivider transform='scaleX(-1) scaleY(1)' />
       <Contact />
+      <Portal to={section}>
+        <Kitty />
+      </Portal>
     </Box>
   );
 }
